51fba9e1836250a3971768658a2f2fc44ce4c527
Carlo Giuseppe Colombo <cgcolombo1983@hotmail.com>
2020-08-26 15:42:41 +0200

    lint control ok

--- js/script.js
+++ js/script.js
@@ -10,24 +10,21 @@ for (var i = 1; i < 101; i++) {
 // 2 Controllo se è divisibile per 15 (5*3)
 	if (i % 15 === 0) {
 		console.log('FizzBuzz');
-	}
 // 3 Controllo se è divisibile per 3
-	else if (i % 3 === 0) {
+	} else if (i % 3 === 0) {
 		console.log('Fizz');
-	}
 // 4 Controllo se è divisibile per 5
-	else if (i % 5 === 0) {
+	}	else if (i % 5 === 0) {
 		console.log('Buzz');
-	}
 // 5 Altrimenti stampo il numero
-	else {
+	}	else {
 		console.log(i);
 	}
 }
 
 // Metodo ternario + falsy e truthy valori
 // Ciclo i numeri da 0 a 100
-for (var i = 0; i < 100;) {
+for (i = 0; i < 100;) {
 // Entro nel ternario con ++i quindi iniziando da 1 e finendo con 100
 // uso il ternario ricordandomi che 0 è "" sono falsy value
 // Sapendo che la valutazione dell'operatore logico va da sx verso destra e se il primo risultato è true Js non guarda il secondo  (ex. ("" + "" \\ 7) = 7) (ex. ("FizzBuzz" || 15) = "FizzBuzz") :
