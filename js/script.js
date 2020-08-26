ca24eb273414adc3b5592796d4d9b9e6a1f79756
Carlo Giuseppe Colombo <cgcolombo1983@hotmail.com>
2020-08-26 15:13:32 +0200

    final

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// FizzBuzz
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz
+
+// Forma lunga
+
+// 1 Ciclo 100 numeri da 1 a 100
+for (var i = 0; i < 101; i++) {
+// 2 Controllo se è divisibile per 15 (5*3)
+	if (i % 15 === 0) {
+		console.log('FizzBuzz');
+	}
+// 3 Controllo se è divisibile per 3
+	else if (i % 3 === 0) {
+		console.log('Fizz');
+	}
+// 4 Controllo se è divisibile per 5
+	else if (i % 5 === 0) {
+		console.log('Buzz');
+	}
+// 5 Altrimenti stampo il numero
+	else {
+		console.log(i);
+	}
+}
+
+// Metodo ternario + falsy e truthy valori
+// Ciclo i numeri da 0 a 100
+for (var i = 0; i < 100;) {
+// Entro nel ternario con ++i quindi iniziando da 1 e finendo con 100
+// uso il ternario ricordandomi che 0 è "" sono falsy value
+// se c'è un false nel primo ternario stampero 'Fizz'
+// se c'è un false nel secondo ternario stamperò 'Buzz'
+// se ci sono due false nei ternari stamperò 'FizzBuzz'
+// altrimenti stamperò il numero console.log(i);
+	console.log((++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || i);
+}
