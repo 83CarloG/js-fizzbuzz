fc65d87e379946894ef8f8faec9141542a8a8f81
Carlo Giuseppe Colombo <cgcolombo1983@hotmail.com>
2020-08-26 15:33:05 +0200

    final

--- js/script.js
+++ js/script.js
@@ -25,15 +25,15 @@ for (var i = 1; i < 101; i++) {
 	}
 }
 
-// // Metodo ternario + falsy e truthy valori
-// // Ciclo i numeri da 0 a 100
-// for (var i = 0; i < 100;) {
-// // Entro nel ternario con ++i quindi iniziando da 1 e finendo con 100
-// // uso il ternario ricordandomi che 0 è "" sono falsy value
-// // Sapendo che la valutazione dell'operatore logico va da sx verso destra e se il primo risultato è true Js non guarda il secondo  (ex. ("" + "" \\ 7) = 7) (ex. ("FizzBuzz" || 15) = "FizzBuzz") :
-// // se c'è un false nel primo ternario stampero 'Fizz'
-// // se c'è un false nel secondo ternario stamperò 'Buzz'
-// // se ci sono due false nei ternari stamperò 'FizzBuzz'
-// // altrimenti stamperò il numero console.log(i);
-// 	console.log((++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || i);
-// }
+// Metodo ternario + falsy e truthy valori
+// Ciclo i numeri da 0 a 100
+for (var i = 0; i < 100;) {
+// Entro nel ternario con ++i quindi iniziando da 1 e finendo con 100
+// uso il ternario ricordandomi che 0 è "" sono falsy value
+// Sapendo che la valutazione dell'operatore logico va da sx verso destra e se il primo risultato è true Js non guarda il secondo  (ex. ("" + "" \\ 7) = 7) (ex. ("FizzBuzz" || 15) = "FizzBuzz") :
+// se c'è un false nel primo ternario stampero 'Fizz'
+// se c'è un false nel secondo ternario stamperò 'Buzz'
+// se ci sono due false nei ternari stamperò 'FizzBuzz'
+// altrimenti stamperò il numero console.log(i);
+	console.log((++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || i);
+}
